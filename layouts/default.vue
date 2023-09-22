73a2f1f324316702eacbd1f260b3f95f8a9f9b6b
Lighuen Restanio <lighuen.restanio@gmail.com>
2023-09-22 21:58:14 +0200

    App created ready to start
    App created ready to start

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-app>
+        <v-app-bar>
+            <v-app-bar-tittle>
+                Swap
+            </v-app-bar-tittle>
+        </v-app-bar>
+        <v-main>
+            <slot />
+        </v-main>
+    </v-app>
+</template>
---
+++ pages/AboutUs.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>AboutUs</h1>
+    </div>
+</template>
