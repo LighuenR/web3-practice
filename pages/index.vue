48b49caabff961b6d3bcbb545a41882e765f6e55
Lighuen Restanio <lighuen.restanio@gmail.com>
2023-09-26 22:14:22 +0200

    Fiel added

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,20 @@
 <template>
     <div>
-        <h1>Test</h1>
+
+        <v-sheet width="400" class="mx-auto">
+            <h2>Send Tokens</h2>
+
+            <v-form>
+                <v-text-field label="Token Amount"></v-text-field>
+            </v-form>
+        </v-sheet>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
