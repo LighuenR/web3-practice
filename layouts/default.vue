174355a53f8874d11a574dced86889a6a4aea518
Lighuen Restanio <lighuen.restanio@gmail.com>
2023-09-27 23:12:54 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,24 @@
                 Swap
             </v-app-bar-tittle>
         </v-app-bar>
+        <v-footer class="bg-grey-lighten-1">
+    <v-row justify="center" no-gutters>
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        variant="text"
+        class="mx-2"
+        rounded="xl"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col class="text-center mt-4" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+    
         <v-main>
             <slot />
         </v-main>
