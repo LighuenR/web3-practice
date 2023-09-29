60adaefa091142b5befc0bbbc6b7c1211efb2e82
Lighuen Restanio <lighuen.restanio@gmail.com>
2023-09-29 23:20:16 +0200

    Plugin added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,22 @@
 <template>
-    <v-app>
-        <v-app-bar>
-            <v-app-bar-title>
-                Swap
-            </v-app-bar-title>
-        </v-app-bar>
-        <v-footer class="bg-grey-lighten-1" app>
-    <v-row justify="center" no-gutters>
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="white"
-        variant="text"
-        class="mx-2"
-        rounded="xl"
-      >
-        {{ link }}
-      </v-btn>
-      <v-col class="text-center mt-4" cols="12">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </v-col>
-    </v-row>
-  </v-footer>
-    
-        <v-main>
-            <slot />
-        </v-main>
-    </v-app>
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-title>
+        Swap
+      </v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <slot />
+    </v-main>
+    <v-footer class="bg-grey-lighten-1" app>
+      <v-row justify="center" no-gutters>
+        <v-btn v-for="link in links" :key="link" color="white" variant="text" class="mx-2" rounded="xl">
+          {{ link }}
+        </v-btn>
+        <v-col class="text-center mt-4" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,22 @@
 <template>
-    <v-container class="d-flex justify-space-around">
-        <v-sheet width="500" class="mx-auto">
-            <h1>Send yourself native tokens
-                Why? There is no why.
-                Balance:</h1>
-        </v-sheet>
-        <v-sheet width="500" class="mx-auto">
-            <v-card title="Send Tokens">
-                <v-form class="mx-auto">
-                    <v-text-field  label="Token Amount" variant="outlined" class="mx-2"></v-text-field>
-                </v-form>
-                <v-card-actions>
-                    <v-btn append-icon="$vuetify" variant="tonal" block>Send</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-sheet>
+    <v-container>
+        <v-row>
+            <v-col cols="12" md="6">
+                <h1>Send yourself native tokens
+                    Why? There is no why.
+                    Balance:</h1>
+            </v-col>
+            <v-col cols="12" md="6">
+                <v-card title="Send Tokens">
+                    <v-form class="mx-auto">
+                        <v-text-field label="Token Amount" variant="outlined" class="mx-2"></v-text-field>
+                    </v-form>
+                    <v-card-actions>
+                        <v-btn append-icon="$vuetify" variant="tonal" block>Send</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
