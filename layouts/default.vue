e1380e7fee733c235db633bfc84950fb906eeebe
Lighuen Restanio <lighuen.restanio@gmail.com>
2023-10-02 22:25:54 +0200

    Darkmode styled ready

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app class="back">
     <v-app-bar>
       <v-app-bar-title>
         Swap
@@ -9,15 +9,17 @@
     <v-main>
       <slot />
     </v-main>
-    <v-footer class="bg-grey-lighten-1" app>
-      <v-row justify="center" no-gutters>
-        <v-btn v-for="link in links" :key="link" color="white" variant="text" class="mx-2" rounded="xl">
-          {{ link }}
-        </v-btn>
-        <v-col class="text-center mt-4" cols="12">
-          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-        </v-col>
-      </v-row>
+
+    <v-footer class="bg-white justify-center" app>
+
+      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+
     </v-footer>
+
+
   </v-app>
 </template>
+<style>
+
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,64 @@
 <template>
     <v-container>
-        <v-row>
+        <v-row class="mt-6">
             <v-col cols="12" md="6">
-                <h1>Send yourself native tokens
+
+                <h1 class="white--text">Send yourself native tokens
                     Why? There is no why.
                     Balance:</h1>
+
             </v-col>
             <v-col cols="12" md="6">
                 <v-card title="Send Tokens">
                     <v-form class="mx-auto">
-                        <v-text-field label="Token Amount" variant="outlined" class="mx-2"></v-text-field>
+                        <v-text-field label="You Pay" variant="outlined" class="mx-2">
+                            <template v-slot:append-inner>
+                                <v-menu>
+                                    <template v-slot:activator="{ props }">
+                                        <v-btn v-bind="props" class="mt-n2">
+                                            <v-icon icon="mdi-menu" start></v-icon>
+
+                                            Menu
+                                        </v-btn>
+                                    </template>
+
+                                    <v-card>
+                                        <v-card-text class="pa-6">
+                                            <v-btn color="primary" size="large" variant="text" @click="clickMe">
+                                                <v-icon icon="mdi-target" start></v-icon>
+
+                                                Click me
+                                            </v-btn>
+                                        </v-card-text>
+                                    </v-card>
+                                </v-menu>
+                            </template>
+                        </v-text-field>
+                    </v-form>
+                    <v-form class="mx-auto">
+                        <v-text-field label="You get" variant="outlined" class="mx-2">
+                            <template v-slot:append-inner>
+                                <v-menu>
+                                    <template v-slot:activator="{ props }">
+                                        <v-btn v-bind="props" class="mt-n2">
+                                            <v-icon icon="mdi-menu" start></v-icon>
+
+                                            Menu
+                                        </v-btn>
+                                    </template>
+
+                                    <v-card>
+                                        <v-card-text class="pa-6">
+                                            <v-btn color="primary" size="large" variant="text" @click="clickMe">
+                                                <v-icon icon="mdi-target" start></v-icon>
+
+                                                Click me
+                                            </v-btn>
+                                        </v-card-text>
+                                    </v-card>
+                                </v-menu>
+                            </template>
+                        </v-text-field>
                     </v-form>
                     <v-card-actions>
                         <v-btn append-icon="$vuetify" variant="tonal" block>Send</v-btn>
@@ -19,11 +68,3 @@
         </v-row>
     </v-container>
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style lang="scss" scoped></style>
