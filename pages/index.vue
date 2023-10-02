2e41ffb0dd1d1792da55e5227549bcb3c4df6eb2
Lighuen Restanio <lighuen.restanio@gmail.com>
2023-10-02 22:28:42 +0200

    Dialog added

--- pages/index.vue
+++ pages/index.vue
@@ -15,11 +15,28 @@
                             <template v-slot:append-inner>
                                 <v-menu>
                                     <template v-slot:activator="{ props }">
-                                        <v-btn v-bind="props" class="mt-n2">
-                                            <v-icon icon="mdi-menu" start></v-icon>
+                                        <v-dialog width="500">
+  <template v-slot:activator="{ props }">
+    <v-btn v-bind="props" text="Open Dialog"> </v-btn>
+  </template>
 
-                                            Menu
-                                        </v-btn>
+  <template v-slot:default="{ isActive }">
+    <v-card title="Dialog">
+      <v-card-text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          text="Close Dialog"
+          @click="isActive.value = false"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+</v-dialog>
                                     </template>
 
                                     <v-card>
@@ -40,11 +57,28 @@
                             <template v-slot:append-inner>
                                 <v-menu>
                                     <template v-slot:activator="{ props }">
-                                        <v-btn v-bind="props" class="mt-n2">
-                                            <v-icon icon="mdi-menu" start></v-icon>
+                                        <v-dialog width="500">
+  <template v-slot:activator="{ props }">
+    <v-btn v-bind="props" text="Open Dialog"> </v-btn>
+  </template>
 
-                                            Menu
-                                        </v-btn>
+  <template v-slot:default="{ isActive }">
+    <v-card title="Dialog">
+      <v-card-text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          text="Close Dialog"
+          @click="isActive.value = false"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+</v-dialog>
                                     </template>
 
                                     <v-card>
