6ed1f37dc4d70afb8fe9a43846baa0a2ef40befa
Lighuen Restanio <lighuen.restanio@gmail.com>
2023-10-04 22:03:36 +0200

    styling text fild and fixing code

--- pages/index.vue
+++ pages/index.vue
@@ -1,100 +1,95 @@
 <template>
     <v-container>
-        <v-row class="mt-6">
-            <v-col cols="12" md="6">
-
-                <h1 class="white--text">Send yourself native tokens
-                    Why? There is no why.
-                    Balance:</h1>
-
-            </v-col>
-            <v-col cols="12" md="6">
+        <v-row class="mt-2 d-flex justify-center">
+            <v-col style="max-width: 500px;">
                 <v-card title="Send Tokens">
-                    <v-form class="mx-auto">
-                        <v-text-field label="You Pay" variant="outlined" class="mx-2">
-                            <template v-slot:append-inner>
-                                <v-menu>
-                                    <template v-slot:activator="{ props }">
-                                        <v-dialog width="500">
-                                            <template v-slot:activator="{ props }">
-                                                <v-btn v-bind="props" text="Open Dialog"> </v-btn>
-                                            </template>
+                    <v-card-text>
+                        <v-form class="mx-auto">
+                            <v-text-field label="You Pay" variant="outlined" class="swap-input">
+                                <template v-slot:append-inner>
+                                    <v-menu>
+                                        <template v-slot:activator="{ props }">
+                                            <v-dialog width="500">
+                                                <template v-slot:activator="{ props }">
+                                                    <v-btn v-bind="props" text="Open Dialog"> </v-btn>
+                                                </template>
 
-                                            <template v-slot:default="{ isActive }">
-                                                <v-card title="Dialog">
-                                                    <v-list :items="items" item-props lines="three">
-                                                        <template v-slot:subtitle="{ subtitle }">
-                                                            <div v-html="subtitle"></div>
-                                                        </template>
-                                                    </v-list>
+                                                <template v-slot:default="{ isActive }">
+                                                    <v-card title="Dialog">
+                                                        <v-list :items="items" item-props lines="three">
+                                                            <template v-slot:subtitle="{ subtitle }">
+                                                                <div v-html="subtitle"></div>
+                                                            </template>
+                                                        </v-list>
 
-                                                    <v-card-actions>
-                                                        <v-spacer></v-spacer>
+                                                        <v-card-actions>
+                                                            <v-spacer></v-spacer>
 
-                                                        <v-btn text="Close Dialog" @click="isActive.value = false"></v-btn>
-                                                    </v-card-actions>
-                                                </v-card>
-                                            </template>
-                                        </v-dialog>
-                                    </template>
+                                                            <v-btn text="Close Dialog"
+                                                                @click="isActive.value = false"></v-btn>
+                                                        </v-card-actions>
+                                                    </v-card>
+                                                </template>
+                                            </v-dialog>
+                                        </template>
 
-                                    <v-card>
-                                        <v-card-text class="pa-6">
-                                            <v-btn color="primary" size="large" variant="text" @click="clickMe">
-                                                <v-icon icon="mdi-target" start></v-icon>
+                                        <v-card>
+                                            <v-card-text class="pa-6">
+                                                <v-btn color="primary" size="large" variant="text" @click="clickMe">
+                                                    <v-icon icon="mdi-target" start></v-icon>
 
-                                                Click me
-                                            </v-btn>
-                                        </v-card-text>
-                                    </v-card>
-                                </v-menu>
-                            </template>
-                        </v-text-field>
-                    </v-form>
-                    <v-form class="mx-auto">
-                        <v-text-field label="You Pay" variant="outlined" class="mx-2">
-                            <template v-slot:append-inner>
-                                <v-menu>
-                                    <template v-slot:activator="{ props }">
-                                        <v-dialog width="500">
-                                            <template v-slot:activator="{ props }">
-                                                <v-btn v-bind="props" text="Open Dialog"> </v-btn>
-                                            </template>
+                                                    Click me
+                                                </v-btn>
+                                            </v-card-text>
+                                        </v-card>
+                                    </v-menu>
+                                </template>
+                            </v-text-field>
+                            <v-text-field label="You Pay" variant="outlined" class="swap-input">
+                                <template v-slot:append-inner>
+                                    <v-menu>
+                                        <template v-slot:activator="{ props }">
+                                            <v-dialog width="500">
+                                                <template v-slot:activator="{ props }">
+                                                    <v-btn v-bind="props" text="Open Dialog"> </v-btn>
+                                                </template>
 
-                                            <template v-slot:default="{ isActive }">
-                                                <v-card title="Dialog">
-                                                    <v-list :items="items" item-props lines="three">
-                                                        <template v-slot:subtitle="{ subtitle }">
-                                                            <div v-html="subtitle"></div>
-                                                        </template>
-                                                    </v-list>
+                                                <template v-slot:default="{ isActive }">
+                                                    <v-card title="Dialog">
+                                                        <v-list :items="items" item-props lines="three">
+                                                            <template v-slot:subtitle="{ subtitle }">
+                                                                <div v-html="subtitle"></div>
+                                                            </template>
+                                                        </v-list>
 
-                                                    <v-card-actions>
-                                                        <v-spacer></v-spacer>
+                                                        <v-card-actions>
+                                                            <v-spacer></v-spacer>
 
-                                                        <v-btn text="Close Dialog" @click="isActive.value = false"></v-btn>
-                                                    </v-card-actions>
-                                                </v-card>
-                                            </template>
-                                        </v-dialog>
-                                    </template>
+                                                            <v-btn text="Close Dialog"
+                                                                @click="isActive.value = false"></v-btn>
+                                                        </v-card-actions>
+                                                    </v-card>
+                                                </template>
+                                            </v-dialog>
+                                        </template>
 
-                                    <v-card>
-                                        <v-card-text class="pa-6">
-                                            <v-btn color="primary" size="large" variant="text" @click="clickMe">
-                                                <v-icon icon="mdi-target" start></v-icon>
+                                        <v-card>
+                                            <v-card-text class="pa-6">
+                                                <v-btn color="primary" size="large" variant="text" @click="clickMe">
+                                                    <v-icon icon="mdi-target" start></v-icon>
 
-                                                Click me
-                                            </v-btn>
-                                        </v-card-text>
-                                    </v-card>
-                                </v-menu>
-                            </template>
-                        </v-text-field>
-                    </v-form>
-                   
+                                                    Click me
+                                                </v-btn>
+                                            </v-card-text>
+                                        </v-card>
+                                    </v-menu>
+                                </template>
+                            </v-text-field>
+                        </v-form>
+                    </v-card-text>
+
                     <v-card-actions>
-                        <v-btn append-icon="$vuetify" variant="tonal" block>Send</v-btn>
+                        <v-btn append-icon="$vuetify" variant="tonal" block size="large">Send</v-btn>
                     </v-card-actions>
                 </v-card>
             </v-col>
@@ -135,3 +130,14 @@ const items = [
     },
 ]
 </script>
+
+<style lang="scss">
+.swap-input {
+    height: 130px;
+
+    input {
+        font-size: 2rem;
+
+    }
+}
+</style>
